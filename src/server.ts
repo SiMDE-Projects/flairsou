d895b07b4143d982aec9fbad2ed56b6b44b184ef
Rémy Huet <perso@remyhuet.fr>
2020-12-08 17:47:03 +0100

    Petit coup de lint

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
 import express, { Request, Response } from 'express';
 
 export default class Server {
-readonly port: number
+  readonly port: number
 
-constructor(port: number) {
-  this.port = port;
-}
+  constructor(port: number) {
+    this.port = port;
+  }
 
-start() {
-  const app = express();
-  app.get('/', (req: Request, res: Response) => {
-    res.send('Hello world !');
-  });
-  app.listen(this.port, () => {
-    console.log('Server started !');
-  });
-}
+  start() {
+    const app = express();
+    app.get('/', (req: Request, res: Response) => {
+      res.send('Hello world !');
+    });
+    app.listen(this.port, () => {
+      console.log('Server started !');
+    });
+  }
 }
