6022e8318edd7be934d7fc162660c032265d6ea0
huetremy <pro@remyhuet.fr>
2020-12-10 12:25:09 +0100

    Corrections review César
    Todo trouver moyen de lint automatiquement les fins de lignes

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   coveragePathIgnorePatterns: [
-    "/node_modules/",
-    "/dist/",
+    '/node_modules/',
+    '/dist/',
   ],
 };
