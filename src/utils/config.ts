5a9bc2062eff5e48670fce43514701af09b3fa13
Rémy Huet <perso@remyhuet.fr>
2020-12-18 22:08:53 +0100

    Ajout de la config DB

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,20 @@
 import { load } from 'ts-dotenv';
+import debug from 'debug';
+
+const log = debug('flairsou:config');
+
+log('Loading environment variables');
 
 const env = load({
   SERVER_PORT: Number,
+  DB_HOST: String,
+  DB_PORT: Number,
+  DB_NAME: String,
+  DB_USER: String,
+  DB_PASSWD: String,
 });
+
+log('Environment variables loaded:');
+log(env);
 
 export default env;
